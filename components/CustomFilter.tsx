1a132638a0bf438793386c30d1df90004212322b
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-07-24 20:05:45 +0530

    work done till 24/07/23

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import { CustomFilterProps } from "@/types"
+
+const CustomFilter = ({title,options}:CustomFilterProps) => {
+  return (
+    <div>
+      Custom Filter
+    </div>
+  )
+}
+
+export default CustomFilter
---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,13 @@
+import { Combobox, Transition } from "@headlessui/react"
+
+import { SearchManuFacturerProps } from "@/types"
+
+const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManuFacturerProps) => {
+  return (
+    <div>
+      SearchManufacturer
+    </div>
+  )
+}
+
+export default SearchManufacturer
